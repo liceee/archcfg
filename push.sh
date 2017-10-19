d5164166dbc25860321e45b3b89c27b1bd46d469
liceee <nangongsnow@gmail.com>
2017-10-20 06:29:56 +0800

    1

--- push.sh
+++ push.sh
@@ -12,42 +12,34 @@ cp /home/lee/.zshrc /bigD/git/archcfg/home/lee/
 cp /home/lee/.emacs /bigD/git/archcfg/home/lee/
 #cp /home/lee/.emacs.d ./home/lee/.emacs.d -r
 
-if [ ! -d "/bigD/git/archcfg/home/lee/.config/chromium" ]; then
-    sleep 0.1
-    echo "/bigD/git/archcfg/home/lee/.config/chromium 目录不存在"
-fi
+if [ -d "/bigD/git/archcfg/home/lee/.config/chromium" ]; then
     sleep 0.1
     echo "/bigD/git/archcfg/home/lee/.config/chromium 目录存在"
     echo "执行删除"
     rm /bigD/git/archcfg/home/lee/.config/chromium -r
-
-
-if [ ! -d "/bigD/git/archcfg/home/lee/.config/kuwo" ]; then
-    sleep 0.1
-    echo "/bigD/git/archcfg/home/lee/.config/kuwo 目录不存在"
 fi
+
+
+if [ -d "/bigD/git/archcfg/home/lee/.config/kuwo" ]; then
     sleep 0.1
     echo "/bigD/git/archcfg/home/lee/.config/kuwo 目录存在"
     echo "执行删除"
     rm /bigD/git/archcfg/home/lee/.config/kuwo -r
-
-if [ ! -d "/bigD/git/archcfg/home/lee/.config/VirtualBox" ]; then
-    sleep 0.1
-    echo "/bigD/git/archcfg/home/lee/.config/VirtualBox 目录不存在"
 fi
+
+if [ -d "/bigD/git/archcfg/home/lee/.config/VirtualBox" ]; then
     sleep 0.1
     echo "/bigD/git/archcfg/home/lee/.config/VirtualBox 目录存在"
     echo "执行删除"
     rm /bigD/git/archcfg/home/lee/.config/VirtualBox -r
-
-if [ ! -d "/bigD/git/archcfg/home/lee/.emacs.d" ]; then
-    sleep 0.1
-    echo "/bigD/git/archcfg/home/lee/.emacs.d 目录不存在"
 fi
+
+if [ -d "/bigD/git/archcfg/home/lee/.emacs.d" ]; then
     sleep 0.1
     echo "/bigD/git/archcfg/home/lee/.emacs.d 目录存在"
     echo "执行删除"
     rm home/lee/.emacs.d -r
+fi
 
     if [ ! -d "/bigD/git/archcfg/home/lee/.config/chromium" ]; then
 	if [ ! -d "/bigD/git/archcfg/home/lee/.config/kuwo" ]; then
@@ -59,20 +51,16 @@ fi
 	    fi
 	fi
     fi
-
-    echo "*****************************加up参数 执行文件上传****************************"
-for arg in "$1"
-do
-    echo $arg
-done
-
-echo $arg
-
-if [ "$arg"x = "up"x ]; then
-	echo "ok"
+    echo
+    echo
+    echo
+    echo "*****************************afp up 版本号 执行文件上传****************************"
+    echo $1
+    if [ -n "$1" ]; then
+	echo "执行提交步骤"
 	cd /bigD/git/archcfg
 	git add .
-	git commit -m "$2"
+	git commit -m "$1"
 	git push
     fi
     exit 0
